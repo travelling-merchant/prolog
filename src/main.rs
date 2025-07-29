c0c6ff04b4047c76a19862f3615dad38b487ee7d
birds-are-corrupted <69503678+birds-are-corrupted@users.noreply.github.com>
2025-07-29 23:37:37 +0200

    I GOT NOT FUCKING TIME TO DO ANYTHING

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,45 @@
-use std::io;
-fn main() {
-    let title_set_theory = "Set Theory ( Mengenlehre )";
-    println!("{}", title_set_theory);
-    println!("{}", ASCII_TITLE_SCREEN);
-    let mut input = String::new();
-    match io::stdin().read_line(&mut input) {
-        Ok(n) => {
-            println!("You wrote {} bytes.", n);
-            println!("{}", ASCII_TEST);
-            let trimmed_input = input.trim_end();
-            println!("Hello, {}!", trimmed_input);
-        }
-        Err(e) => {
-            eprintln!("Error reading line: {}", e);
+use crossterm::event::{self, Event, KeyCode, KeyEventKind};
+use ratatui::{Frame, text::Text};
+#[derive(PartialEq)]
+enum InputCommands {
+    ContinueAction,
+    Quit,
+}
+fn main() -> std::io::Result<()> {
+    let mut terminal = ratatui::init();
+    let result = run(&mut terminal);
+    ratatui::restore();
+    result
+}
+fn run(terminal: &mut ratatui::DefaultTerminal) -> std::io::Result<()> {
+    loop {
+        terminal.draw(|frame| draw_title_screen(frame))?;
+        let input_result = handle_events();
+        match input_result {
+            Ok(result_here) => {
+                if result_here == InputCommands::Quit {
+                    break Ok(());
+                }
+                if result_here == InputCommands::ContinueAction {
+                    terminal.draw(|frame| n(frame))?;
+                }
+            }
+            Err(e) => {
+                break Err(e);
+            }
         }
     }
 }
-const ASCII_TEST: &str = r#"
-⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⣠⣴⡿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣷⣤⡀⠀
-⠀⠀⢀⣾⡟⡍⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⡙⣿⡄
-⠀⠀⣸⣿⠃⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠇⣹⣿
-⠀⠀⣿⣿⡆⢚⢄⣀⣠⠤⠒⠈⠁⠀⠀⠈⠉⠐⠢⢄⡀⣀⢞⠀⣾⣿
-⠀⠀⠸⣿⣿⣅⠄⠙⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡟⠑⣄⣽⣿⡟
-⠀⠀⠀⠘⢿⣿⣟⡾⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠱⣾⣿⣿⠏⠀
-⠀⠀⠀⠀⣸⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡉⢻⠀⠀
-⠀⠀⠀⠀⢿⠀⢃⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠁⢸⠀⠀
-⠀⠀⠀⠀⢸⢰⡿⢘⣦⣤⣀⠑⢦⡀⠀⣠⠖⣁⣤⣴⡊⢸⡇⡼⠀⠀
-⠀⠀⠀⠀⠀⠾⡅⣿⣿⣿⣿⣿⠌⠁⠀⠁⢺⣿⣿⣿⣿⠆⣇⠃⠀⠀
-⠀⠀⠀⠀⠀⢀⠂⠘⢿⣿⣿⡿⠀⣰⣦⠀⠸⣿⣿⡿⠋⠈⢀⠀⠀⠀
-⠀⠀⠀⠀⠀⢠⠀⠀⠀⠀⠀⠀⢠⣿⢻⣆⠀⠀⠀⠀⠀⠀⣸⠀⠀⠀
-⠀⠀⠀⠀⠀⠈⠓⠶⣶⣦⠤⠀⠘⠋⠘⠋⠀⠠⣴⣶⡶⠞⠃⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⣿⢹⣷⠦⢀⠤⡤⡆⡤⣶⣿⢸⠇⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⢰⡀⠘⢯⣳⢶⠦⣧⢷⢗⣫⠇⠀⡸⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠑⢤⡀⠈⠋⠛⠛⠋⠉⢀⡠⠒⠁⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⢦⠀⢀⣀⠀⣠⠞⠁⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-"#;
+fn handle_events() -> std::io::Result<InputCommands> {
+    match event::read()? {
+        Event::Key(key) if key.kind == KeyEventKind::Press => match key.code {
+            KeyCode::Char('q') => return Ok(InputCommands::Quit),
+            _ => {}
+        },
+        _ => {}
+    }
+    Ok(InputCommands::ContinueAction)
+}
 
 const ASCII_TITLE_SCREEN: &str = r#"
                 .                                            .
@@ -64,3 +66,12 @@ ____^/\___^--____/\____O______________/\/\---/\___________---______________
    --  __                      ___--  ^  ^                         --  __
 
 "#;
+
+fn draw_title_screen(frame: &mut Frame) {
+    let text = Text::raw(ASCII_TITLE_SCREEN);
+    frame.render_widget(text, frame.area());
+}
+fn n(frame: &mut Frame) {
+    let text = Text::raw("n");
+    frame.render_widget(text, frame.area());
+}
